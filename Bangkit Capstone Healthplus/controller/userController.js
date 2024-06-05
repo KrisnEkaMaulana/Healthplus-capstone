839a4bc0da0b4902fe467a036c2fe4c97d809de1
JustinGarretYan <kronixyan@gmail.com>
2024-06-05 13:20:33 +0800

    nyoba express.js

---
+++ Bangkit Capstone Healthplus/controller/userController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    getAllUsers: (req, res) => {
+        res.send("data user")
+    },
+    login: (req, res) => {
+        res.send("login")
+    },
+    register: (req, res) => {
+        res.send("regis")
+    },
+}
